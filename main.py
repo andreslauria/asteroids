59670813a98c0d68633bbceb666ccd2322a5fce5
andreslauria <andreslauria@gmail.com>
2025-05-01 02:59:09 -0300

    ch2 L4

--- main.py
+++ main.py
@@ -11,12 +11,18 @@ def main():
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode(
         (constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    timer = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+        delta = timer.tick(60)
+        dt = delta/1000
 
 
 if __name__ == "__main__":
